cb42955fdb4003a1fe89b7a6388f275da123b919
ardyassetya <72063228+Ardyas-setya@users.noreply.github.com>
2021-12-28 23:22:12 +0700

    update backend- eror nav

--- application/views/admin/layout/v_footer.php
+++ application/views/admin/layout/v_footer.php
@@ -1,17 +1,18 @@
-</div>
+
+        </div>
         <!-- /#wrapper -->
 
         <!-- jQuery -->
-        <script src="<?= base_url()?>template/back-end/js/jquery.min.js"></script>
+        <script src="<?= base_url() ?>template/back-end/js/jquery.min.js"></script>
 
         <!-- Bootstrap Core JavaScript -->
-        <script src="<?= base_url()?>template/back-end/js/bootstrap.min.js"></script>
+        <script src="<?= base_url() ?>template/back-end/js/bootstrap.min.js"></script>
 
         <!-- Metis Menu Plugin JavaScript -->
-        <script src="<?= base_url()?>template/back-end/js/metisMenu.min.js"></script>
+        <script src="<?= base_url() ?>template/back-end/js/metisMenu.min.js"></script>
 
         <!-- Custom Theme JavaScript -->
-        <script src="<?= base_url()?>template/back-end/js/startmin.js"></script>
+        <script src="<?= base_url() ?>template/back-end/js/startmin.js"></script>
 
     </body>
 </html>
--- application/views/admin/layout/v_head.php
+++ application/views/admin/layout/v_head.php
@@ -7,19 +7,19 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>MI MUHAMMADIYAH KALIPEPE - <?= $title ?></title>
+        <title>MI KALIPEPE - <?= $title ?></title>
 
         <!-- Bootstrap Core CSS -->
-        <link href="<?= base_url()?>template/back-end/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>template/back-end/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- MetisMenu CSS -->
-        <link href="<?= base_url()?>template/back-end/css/metisMenu.min.css" rel="stylesheet">
+        <link href="<?= base_url() ?>template/back-end/css/metisMenu.min.css" rel="stylesheet">
 
         <!-- Custom CSS -->
-        <link href="<?= base_url()?>template/back-end/css/startmin.css" rel="stylesheet">
+        <link href="<?= base_url() ?>template/back-end/css/startmin.css" rel="stylesheet">
 
         <!-- Custom Fonts -->
-        <link href="<?= base_url()?>template/back-end/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url() ?>template/back-end/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
